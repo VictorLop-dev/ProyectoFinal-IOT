db65a07d8122718d01dbd23bfcd4781dcca4bf12
VictorLop-dev <a01278045@tec.mx>
2024-11-22 16:18:06 -0600

    Update streamLitV1.py

--- streamLitV1.py
+++ streamLitV1.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 # Conexión a la base de datos
 connection = mysql.connector.connect(
-    host='mysql.railway.internal',
+    host='iotweb-production.up.railway.app',
     user='root',
     password='QYruqXDRGGyBxlYXXcoMmaTSExlNQYxZ',
     database='railway'
