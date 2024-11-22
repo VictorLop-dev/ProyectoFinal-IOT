5f68c9e4a9b2d889586b8fd1aebbaba73c664f71
VictorLop-dev <a01278045@tec.mx>
2024-11-22 16:31:51 -0600

    Update streamLitV1.py

--- streamLitV1.py
+++ streamLitV1.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 # Conexión a la base de datos
 connection = mysql.connector.connect(
-    host='mysql://root:QYruqXDRGGyBxlYXXcoMmaTSExlNQYxZ@autorack.proxy.rlwy.net:12903/railway',
+    host='//root:QYruqXDRGGyBxlYXXcoMmaTSExlNQYxZ@autorack.proxy.rlwy.net:12903/railway',
     user='root',
     password='QYruqXDRGGyBxlYXXcoMmaTSExlNQYxZ',
     database='railway'
