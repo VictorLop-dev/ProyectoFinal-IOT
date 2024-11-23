d42ac93cf7e609b74e580eab4992081d0e500201
VictorLop-dev <a01278045@tec.mx>
2024-11-22 20:05:26 -0600

    Update streamLitV1.py

--- streamLitV1.py
+++ streamLitV1.py
@@ -63,7 +63,7 @@ if not df.empty:
             ax.text(
                 df['timestamp'].iloc[i],
                 df['valor'].iloc[i] - 0.25,
-                f"{df['valor'].iloc[i]:.2f}",
+                f"{df['valor'].iloc[i]}",
                 color='white',
                 fontsize=8,
                 ha='center'
