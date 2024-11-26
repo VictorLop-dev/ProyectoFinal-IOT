f68fff1e3ee0779cf3ad15214b4718dc10aded44
VictorLop-dev <a01278045@tec.mx>
2024-11-26 04:58:53 -0600

    Update final.py

--- final.py
+++ final.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+import pandas as pd
+import matplotlib.pyplot as plt
 # Configuración inicial de la página
 st.set_page_config(page_title="Pokmed App", layout="wide", page_icon="💊")
 
