42fde646716c278e6aee9b26efa59ee9cfa19eae
VictorLop-dev <a01278045@tec.mx>
2024-11-26 04:04:20 -0600

    Update frankenstein.py

--- frankenstein.py
+++ frankenstein.py
@@ -91,8 +91,8 @@ if "page" not in st.session_state:
     
 # Inicio de sesión
 if st.session_state.page == "login":
-    uno,dos,tres,cuatro,cinco,seis,siete = st.columns(7)
-    with cuatro:
+    uno,dos,tres,cuatro,cinco = st.columns(5)
+    with tres:
         st.image("PokMed.jpg", caption="",width=250)
         st.markdown("<h1>¡Bienvenido a Pokmed App!</h1>", unsafe_allow_html=True)
     
