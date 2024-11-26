5d1b9d628fa5a2e55c91fea0843074af2b507366
VictorLop-dev <a01278045@tec.mx>
2024-11-26 03:12:38 -0600

    Update frankenstein.py

--- frankenstein.py
+++ frankenstein.py
@@ -99,7 +99,7 @@ if st.session_state.page == "login":
 
     # Validación de usuario
     if st.button("Iniciar sesión"):
-        if email == "ejemplo@email.com" and password == "12345678":
+        if email == "josepablo83@email.com" and password == "12345678":
             st.session_state.page = "home"
             st.rerun()  # Recarga segura tras cambio de página
         else:
