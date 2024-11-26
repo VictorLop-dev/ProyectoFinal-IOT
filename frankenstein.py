133b19125ccd3ffc5692e3e4a507c4de157f5cb7
VictorLop-dev <a01278045@tec.mx>
2024-11-26 03:29:31 -0600

    Update frankenstein.py

--- frankenstein.py
+++ frankenstein.py
@@ -123,6 +123,7 @@ elif st.session_state.page == "home":
         st.markdown('<div class="box">', unsafe_allow_html=True)
         st.markdown("### Recomendación actual de: **INSULINA**")
         st.markdown('<div class="info-box">Fecha de caducidad: 24/07/25</div>', unsafe_allow_html=True)
+        st.write("")
         st.markdown('<div class="info-box">Fecha de apertura: 24/07/25</div>', unsafe_allow_html=True)
         st.markdown('</div>', unsafe_allow_html=True)
 
