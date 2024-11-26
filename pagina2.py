529113db4a818d685df447355d77135b9da28123
VictorLop-dev <a01278045@tec.mx>
2024-11-26 05:40:21 -0600

    Update pagina2.py

--- pagina2.py
+++ pagina2.py
@@ -74,23 +74,34 @@ def main():
         if 'timestamp' in df.columns:
             df['timestamp'] = pd.to_datetime(df['timestamp'])
             df = df.sort_values(by='timestamp', ascending=True)
-
         # Cargar datos de movimiento
         df2 = fetch_data_mov(host, user, password, database)
         if 'timestamp' in df2.columns:
             df2['timestamp'] = pd.to_datetime(df2['timestamp'])
             df2 = df2.sort_values(by='timestamp', ascending=True)
-
+    
         if df['valor'].iloc[-1] > 26:
-            st.subheader("¡Tu insulina está en malas condiciones en este momento! Su temperatura supera los 26°C")
+            st.subheader("Tu insulina esta en malas condiciones en este momento! La temperatura supera los 26°")
         elif df['valor'].iloc[-1] < 2:
-            st.subheader("¡Tu insulina está en malas condiciones en este momento! Su temperatura está por debajo de los 2°C ")
+            st.subheader("Tu insulina esta en malas condiciones en este momento! La temperatura esta debajo de 2°")
         else:
-            st.subheader("¡Todo parece bien con tu insulina!")
-
+            st.subheader("Todo parece bien con tu insulina!")
+    col1, col2=st.columns(2)
+    with col1:
+        df = fetch_data(host, user, password, database)
+        if 'timestamp' in df.columns:
+            df['timestamp'] = pd.to_datetime(df['timestamp'])
+            df = df.sort_values(by='timestamp', ascending=True)
+        # Cargar datos de movimiento
+        df2 = fetch_data_mov(host, user, password, database)
+        if 'timestamp' in df2.columns:
+            df2['timestamp'] = pd.to_datetime(df2['timestamp'])
+            df2 = df2.sort_values(by='timestamp', ascending=True)
+            
+    
         # Crear la gráfica con puntos y colores personalizados
         fig, ax = plt.subplots()
-
+    
         # Colores según las condiciones
         for i in range(len(df) - 1):  # Iterar sobre los puntos
             # Determinar el color del punto
@@ -100,10 +111,10 @@ def main():
                 color = '#4335A7'
             else:
                 color = '#80C4E9'
-
+    
             # Graficar el punto
             ax.scatter(df['timestamp'].iloc[i], df['valor'].iloc[i], color=color, zorder=3)
-
+    
             # Agregar el valor debajo del punto
             ax.text(
                 df['timestamp'].iloc[i],
@@ -113,7 +124,7 @@ def main():
                 fontsize=8,
                 ha='center'  # Centrar el texto horizontalmente
             )
-
+    
             # Determinar el color de la línea según el punto al que conecta
             if df['valor'].iloc[i + 1] > 26:
                 line_color = '#FF7F3E'
@@ -121,7 +132,7 @@ def main():
                 line_color = '#4335A7'
             else:
                 line_color = '#80C4E9'
-
+    
             # Graficar la línea hasta el siguiente punto
             ax.plot(
                 [df['timestamp'].iloc[i], df['timestamp'].iloc[i + 1]],
@@ -129,7 +140,7 @@ def main():
                 color=line_color,
                 zorder=2
             )
-
+    
         # Graficar el último punto
         if df['valor'].iloc[-1] > 29:
             last_color = '#FF7F3E'
@@ -137,9 +148,9 @@ def main():
             last_color = '#4335A7'
         else:
             last_color = '#80C4E9'
-
+    
         ax.scatter(df['timestamp'].iloc[-1], df['valor'].iloc[-1], color=last_color, zorder=3)
-
+    
         # Agregar el valor al último punto
         ax.text(
             df['timestamp'].iloc[-1],
@@ -149,24 +160,27 @@ def main():
             fontsize=8,
             ha='center'
         )
-
         ax.set_ylim(df['valor'].min() - 1, df['valor'].max() + 1)
+        # Lo de abajo en teoria amplia el alcance de la grafica en x, pero hasta ahora no ha hecho falta.
+        # ax.set_xlim(df['timestamp'].min() - pd.Timedelta(seconds=5), df['timestamp'].max() + pd.Timedelta(seconds=5))  # Espacio horizontal
+        # Ajustes del gráfico
         ax.set_title('Temperatura de la insulina', color='white')  # Título en blanco para destacar
         ax.set_xlabel('Tiempo', color='white')  # Etiqueta del eje x en blanco
         ax.set_ylabel('Temperatura (°C)', color='white')  # Etiqueta del eje y en blanco
         plt.xticks(rotation=45, color='white')  # Rotar etiquetas del eje x y ponerlas en blanco
         plt.yticks(color='white')  # Etiquetas del eje y en blanco
-
+    
         # Invertir el eje x para que las más recientes estén a la izquierda
         ax.invert_xaxis()
-
+    
         # Mostrar la gráfica en Streamlit
         st.pyplot(fig)
-
-        # GRAFICA 2
+        st.subheader("Tabla de Temperaturas")
+        st.dataframe(df)
+    with col2:
         fig2, ax2 = plt.subplots()
 
-        # Colores según las condiciones
+    # Colores según las condiciones
         for i in range(len(df2) - 1):  # Iterar sobre los puntos
             # Determinar el color del punto
             if df2['valor'].iloc[i] > 26:  # Ajustar a tus valores
@@ -175,10 +189,10 @@ def main():
                 color = '#4335A7'
             else:
                 color = '#80C4E9'
-
+    
             # Graficar el punto
             ax2.scatter(df2['timestamp'].iloc[i], df2['valor'].iloc[i], color=color, zorder=3)
-
+    
             # Agregar el valor debajo del punto
             ax2.text(
                 df2['timestamp'].iloc[i],
@@ -188,13 +202,13 @@ def main():
                 fontsize=8,
                 ha='center'  # Centrar el texto horizontalmente
             )
-
+    
             # Determinar el color de la línea según el punto al que conecta
             if df2['valor'].iloc[i + 1] > 2:
                 line_color = '#FF7F3E'
             else:
                 line_color = '#80C4E9'
-
+    
             # Graficar la línea hasta el siguiente punto
             ax2.plot(
                 [df2['timestamp'].iloc[i], df2['timestamp'].iloc[i + 1]],
@@ -202,15 +216,15 @@ def main():
                 color=line_color,
                 zorder=2
             )
-
+    
         # Graficar el último punto
         if df2['valor'].iloc[-1] > 2:
             last_color = '#FF7F3E'
         else:
             last_color = '#80C4E9'
-
+    
         ax2.scatter(df2['timestamp'].iloc[-1], df2['valor'].iloc[-1], color=last_color, zorder=3)
-
+    
         # Agregar el valor al último punto
         ax2.text(
             df2['timestamp'].iloc[-1],
@@ -220,29 +234,23 @@ def main():
             fontsize=8,
             ha='center'
         )
-
         ax2.set_ylim(df2['valor'].min() - 1, df2['valor'].max() + 1)
-        ax2.set_title('Movimientos del sensor', color='white')  # Título en blanco para destacar
+        # Lo de abajo en teoria amplia el alcance de la grafica en x, pero hasta ahora no ha hecho falta.
+        # ax.set_xlim(df['timestamp'].min() - pd.Timedelta(seconds=5), df['timestamp'].max() + pd.Timedelta(seconds=5))  # Espacio horizontal
+        # Ajustes del gráfico
+        ax2.set_title('Movimiento de la insulina', color='white')  # Título en blanco para destacar
         ax2.set_xlabel('Tiempo', color='white')  # Etiqueta del eje x en blanco
-        ax2.set_ylabel('Valores', color='white')  # Etiqueta del eje y en blanco
+        ax2.set_ylabel('Temperatura (°C)', color='white')  # Etiqueta del eje y en blanco
         plt.xticks(rotation=45, color='white')  # Rotar etiquetas del eje x y ponerlas en blanco
         plt.yticks(color='white')  # Etiquetas del eje y en blanco
-
+    
         # Invertir el eje x para que las más recientes estén a la izquierda
         ax2.invert_xaxis()
-
+    
         # Mostrar la gráfica en Streamlit
         st.pyplot(fig2)
-        st.write("")
-        st.write("")
-        st.write("")
-        col1, col2=st.columns(2)
-        with col1:
-            st.subheader("Tabla de Temperaturas")
-            st.dataframe(df)
-        with col2:
-            st.subheader("Tabla de Movimiento")
-            st.dataframe(df2)
+        st.subheader("Tabla de Movimiento")
+        st.dataframe(df2)
     
 if __name__ == "__main__":
     main()
