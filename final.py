877c378023c31d8fe71aaa79b62155ed27a54321
VictorLop-dev <a01278045@tec.mx>
2024-11-26 04:33:53 -0600

    Update final.py

--- final.py
+++ final.py
@@ -101,6 +101,11 @@ if st.session_state.page == "login":
        st.write("")
        st.write("")
        st.write("")
+       st.write("")
+       st.write("")
+       st.write("")
+       st.write("")
+       st.write("")
        st.markdown("<h2>Bienvenido</h2>", unsafe_allow_html=True)
    with cuatro:
     st.image("PokMed.jpg", caption="",width=250)
