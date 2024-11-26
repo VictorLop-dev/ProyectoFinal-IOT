b817f9246ce56b2ab6e45b7b17e73ed7d38d9840
VictorLop-dev <a01278045@tec.mx>
2024-11-26 03:09:29 -0600

    Update frankenstein.py

--- frankenstein.py
+++ frankenstein.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_autorefresh import st_autorefresh
+
 # Configuración inicial de la página
 st.set_page_config(page_title="Pokmed App", layout="wide", page_icon="💊")
 
