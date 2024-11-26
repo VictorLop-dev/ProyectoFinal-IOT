7336b47d94e7cc8f48df45db4edc5c165eebf06e
VictorLop-dev <a01278045@tec.mx>
2024-11-26 05:25:04 -0600

    Update final.py

--- final.py
+++ final.py
@@ -115,7 +115,7 @@ if st.session_state.page == "login":
 
 # Dashboard
 elif st.session_state.page == "home":
-    st.image("PokMed.jpg", caption="",width=150)
+    st.image("PokMed.jpg", caption="",width=100)
     st.markdown('<div class="header">POKMED</div>', unsafe_allow_html=True)
     st.markdown('<div class="sub-header">Bienvenido de vuelta, José Pablo</div>', unsafe_allow_html=True)
     st.write("")
